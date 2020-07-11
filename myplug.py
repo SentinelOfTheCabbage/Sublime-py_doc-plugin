a31d9eb8396bfc1e6f30991169052a0c024842b0
Наиль Сулейманов <nmsuleymanov@miem.hse.ru>
2020-07-11 05:49:19 +0300

    Sublime base options inspecting night =( WTSleep

--- myplug.py
+++ myplug.py
@@ -1,4 +1,5 @@
 # import sublime
+# from sublime import active_window
 # import sublime_plugin
 # import pickle
 # import json
@@ -8,61 +9,33 @@ import os
 plugin_folder = r'C:\Users\Tom\AppData\Roaming\Sublime Text 3\Packages\User'
 filename = 'myplug.py'
 
-
-class ExampleCommand(sublime_plugin.TextCommand):
+# class ExampleCommand(sublime_plugin.TextCommand, sublime.Window, sublime.View):
+class ExampleCommand(sublime_plugin.TextCommand, sublime_plugin.Window, sublime_plugin.View):
 
     def run(self, edit):
-        PATH = r'C:\Users\Tom\Documents\Python_projects\SublimePlugin'
-        ans = self.get_methods(sublime, 'sublime', 0)
-        with open(PATH + r'\sublime.json', 'w') as file:
-            file.write(str(ans))
+        print(self.view.view_id)
+        # print(dir(edit))
+        W = active_window()
+        V = W.active_view()
+        dot = V.viewport_position()
+        with open(plugin_folder+'\\x.txt','w') as file:
+            file.write(str(dot))
+        R = Region
+        # print(W.window_id)
+        # print(W.active_view().window().window_id)
+        # self.view.insert(edit, 0, "Hello, World!")
+        # print(dir(sublime_plugin),sublime_plugin.__file__)
 
-    def get_methods(self, obj: object, parent_obj: str, depth)->():
-        content = dir(obj)
-        result = {}
-        is_module = {}
-        is_hash = {}
-        is_float = {}
-        is_elementary = {}
-        bullshit = [
-            "<class 'int'>",
-            "<class 'float'>",
-            "<class 'str'>",
-            "<class 'list'>",
-            "<class 'dict'>",
-            "<class 'set'>",
-        ]
-
-        if depth < 2:
-            for element in content:
-                exec('is_module["key"] = (str(type(' + parent_obj +
-                     '.' + element + ')) == "<class \'module\'>")')
-                exec('is_float["key"] = (str(type(' + parent_obj +
-                     '.' + element + ')) == "<class \'sys.float_info\'>")')
-                exec('is_hash["key"] = (str(type(' + parent_obj +
-                     '.' + element + ')) == "<class \'sys.hash_info\'>")')
-                exec('is_elementary["key"] = str(type(' +
-                     parent_obj + '.' + element + ')) in bullshit')
-
-                if not (is_hash['key'] or is_float['key']):
-                    if not is_module['key']:
-                        exec('result[\'' + element + '\'] = ' +
-                             parent_obj + '.' + element)
-                    else:
-                        print(parent_obj + '.' + element)
-                        exec('result[\'' + element + '\'] = ' + 'self.get_methods(' + parent_obj + '.' +
-                             element + ', \'' + parent_obj + '.' + element + '\', ' + str(depth + 1) + ')')
-                else:
-                    continue
-        return result
 
 if __name__ == '__main__':
     print(__name__)
     with open(filename, 'r') as file:
         current_code = file.read()
-    current_code = current_code.replace('# import', 'import')
-    current_code = current_code.replace('import replacer as sublime_plugin\n', '')
+    current_code = current_code.replace('import', 'import')
+    current_code = current_code.replace(
+        'import replacer as sublime_plugin\n', '')
 
-    with open('{}\\{}'.format(plugin_folder,filename), 'w') as file:
+    with open('{}\\{}'.format(plugin_folder, filename), 'w') as file:
         file.write(current_code)
     print('File were updated!')
+    print('Write "view.run_command(\'example\')" in console to run it =)')
--- replacer.py
+++ replacer.py
@@ -1,3 +1,9 @@
 class TextCommand():
 	def __init__():
 		pass
+
+class Window():
+	pass
+
+class View():
+	pass
